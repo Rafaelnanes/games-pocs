fc9bd75cb556a05aea47f6461c57afda1ca197f8
Rafael Nanes <rafaelbraga_22@hotmail.com>
2022-09-06 22:18:27 +0100

    added tiled test

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -2,14 +2,15 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.mygdx.game.tutorial5.SceneDemo;
+import com.mygdx.game.tutorial7.TiledTest;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
   public static void main(String[] arg) {
     Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
     config.setForegroundFPS(60);
-    config.setTitle("Nave");
-    new Lwjgl3Application(new SceneDemo(), config);
+    config.setTitle("Sei lá");
+    config.setWindowedMode(1280, 720);
+    new Lwjgl3Application(new TiledTest(), config);
   }
 }
