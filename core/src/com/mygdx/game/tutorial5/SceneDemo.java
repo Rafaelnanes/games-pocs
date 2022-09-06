3525228f44857b138884d30ce36e77f5130fc0fd
Rafael Nanes <rafaelbraga_22@hotmail.com>
2022-09-06 20:17:02 +0100

    first commit

---
+++ core/src/com/mygdx/game/tutorial5/SceneDemo.java
@@ -0,0 +1,55 @@
+package com.mygdx.game.tutorial5;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+public class SceneDemo implements ApplicationListener {
+
+  private Stage stage;
+
+  @Override
+  public void create() {
+    stage = new Stage();
+
+    MyActor myActor = new MyActor();
+    stage.addActor(myActor);
+  }
+
+  @Override
+  public void dispose() {
+    stage.dispose();
+  }
+
+  @Override
+  public void render() {
+    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    stage.draw();
+  }
+
+  @Override
+  public void resize(int width, int height) {
+  }
+
+  @Override
+  public void pause() {
+  }
+
+  @Override
+  public void resume() {
+  }
+
+
+  public class MyActor extends Actor {
+    Texture texture = new Texture(Gdx.files.internal("data/jet.png"));
+
+    @Override
+    public void draw(Batch batch, float alpha) {
+      batch.draw(texture, 0, 0);
+    }
+  }
+}
